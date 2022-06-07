5d91b9f6e5b76bd69caf97c94420dd36f953e9c6
David Matheson <davidfmatheson@gmail.com>
2022-06-07 12:56:17 -0400

    Updating nx

--- apps/batsignal/jest.config.ts
+++ apps/batsignal/jest.config.ts
@@ -1,5 +1,6 @@
-module.exports = {
-  displayName: 'ui',
+/* eslint-disable */
+export default {
+  displayName: 'batsignal',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +9,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/libs/ui',
+  coverageDirectory: '../../coverage/apps/batsignal',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
--- libs/ui/jest.config.ts
+++ libs/ui/jest.config.ts
@@ -1,5 +1,6 @@
-module.exports = {
-  displayName: 'batsignal',
+/* eslint-disable */
+export default {
+  displayName: 'ui',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +9,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/batsignal',
+  coverageDirectory: '../../coverage/libs/ui',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
