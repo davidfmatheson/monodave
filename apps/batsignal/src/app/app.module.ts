2999b2c0d0ee528634b09aa5e6bd808701a04b9c
David Matheson <davidfmatheson@gmail.com>
2022-06-13 15:25:05 -0400

    Add angular material

--- apps/batsignal/src/app/app.module.ts
+++ apps/batsignal/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UiModule } from '@monodave/ui';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule, UiModule],
+  imports: [BrowserModule, HttpClientModule, UiModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
